e02a69300bf0b273e4802122c01b1b5bc00e40ac
Maxim <maxmaxmaxmaxmaxa@mail.ru>
2023-03-09 23:38:52 +0300

    lection 1

---
+++ java_project/program.java
@@ -0,0 +1,11 @@
+package java_project;
+
+
+public class program {
+    public static void main(String[] args) {
+        float f = 123f;
+        System.out.println(f);  // вывод на терминал
+    }
+    
+}
+
