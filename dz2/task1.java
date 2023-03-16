e2318fda28fe22fe90bc4c578be5e4734b2778c1
Maxim <maxmaxmaxmaxmaxa@mail.ru>
2023-03-16 13:15:15 +0300

    version 2, task 1

--- dz2/task1.java
+++ dz2/task1.java
@@ -28,43 +28,40 @@ public class task1
                 }
             }
         }
-    }        // fr.write(sb.toString());
-    // fr.write("\n");
-    // fr.close();
+    }
 
     
-    public static void main(String[] args) 
+    public static void main(String[] args) throws Exception
     {
-        File file = new File("array.txt");
-        StringBuilder sb = new StringBuilder();
-        
+        File file = new File("array.txt"); //Создание файла в который записывается массив
+        StringBuilder sb = new StringBuilder(); 
         Scanner input = new Scanner(System.in);
-        System.out.println("Введите длину массива: ");
+        System.out.println("Введите длину массива: "); //Ввод длины массива с клавиатуры
         int size = input.nextInt(); 
-        int array[] = new int[size]; 
-        System.out.println("Введите элементы массива: ");
-        for (int i = 0; i < size; i++) 
+        int array[] = new int[size];  // Создание массива
+        System.out.println("Введите элементы массива: "); // Ввод элементов Массива с клавиатуры 
+        for (int i = 0; i < size; i++)  
         {
             
             array[i] = input.nextInt();
         }
         for (int i = 0; i < size; i++) 
         {
-            bubbleSort(array);
             sb.append(" " + array[i]);
-            
         }
-        try
-        {
-        FileWriter fr = new FileWriter(file, true);
-        
-        fr.write(sb.toString());
-        fr.write("\n");
-        fr.close();
-        }
-        catch (IOException e)
-        {
-            System.out.println("ERROR");
-        }
+            FileWriter fr = new FileWriter(file, true);
+            fr.write(sb.toString());
+            fr.write("\n");
+            fr.close();
+            for (int i = 0; i < size; i++)
+            {
+                
+                bubbleSort(array);
+                sb.append(" " + array[i]);
+                FileWriter fr2 = new FileWriter(file, true);
+                fr2.write(sb.toString());
+                fr2.write("\n");
+                fr2.close();
+            } 
     }
 }
