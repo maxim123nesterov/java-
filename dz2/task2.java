f692623fbfacf298106c5c421791f756763d244d
Maxim <maxmaxmaxmaxmaxa@mail.ru>
2023-03-18 15:42:29 +0300

    trash

--- dz2/task2.java
+++ dz2/task2.java
@@ -1,34 +1,59 @@
 package dz2;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.Scanner;
 
-public class task2 
+public class task2  
 {
-    public static void EnterName() 
+    public static void main(String[] args) throws Exception 
     {
-        Scanner iScanner = new Scanner(System.in, "Cp866");
-        System.out.println("Введите фамилию ученика: ");
-        String name = iScanner.next();
-        System.out.println(name);
+        String [] arrayData = lib.ReadLineFromFilee("dataForTask2.txt");
+        for(int i = 0; i < arrayData.length; i++) 
+        {
+            System.out.println(PrintLine(arrayData[i]));
+            
+        }
     }
-    public static void EnterLess()
+        
+            public static String [] ReadLineFromFile(String pathway) throws Exception 
+            {
+                BufferedReader br = new BufferedReader(new FileReader(pathway));
+                String str;
+                int size = 0;
+                while ((str = br.readLine()) != null) 
+                {
+                    size +=1;
+                }
+                br.close();
+                String [] listData = new String [size];
+               
+                int i = 0;
+                BufferedReader br1 = new BufferedReader(new FileReader(pathway));
+                while ((str = br1.readLine()) != null) 
+                {
+                    listData[i] = str;
+                    i += 1;
+                }
+                br1.close();
+                return listData;
+            
+            }   
+    
+    public static StringBuilder PrintLine(String line) 
     {
-        Scanner iScanner = new Scanner(System.in, "Cp866");
-        System.out.println("Введите предмет: ");
-        String less = iScanner.nextLine();
-        System.out.println(less);
-    }
-    public static void EnterVal()
-    {
-        Scanner iScanner = new Scanner(System.in, "Cp866");
-        System.out.println("Введите оценку: ");
-        String val = iScanner.nextLine();
-        System.out.println(val);
-    }
-    public static void main(String[] args) 
-    {
-        EnterName();
-        EnterLess();
-        EnterVal();
+        String line1 = line.replace("{", "");
+        String line2 = line1.replace("}", "");
+        String line3 = line2.replaceAll("\"", "");
+        String line4 = line3.replace("[", "");
+        String line5 = line4.replace("]", "");
+        StringBuilder result = new StringBuilder("");
+        String [] arrayData = line5.split(",");
+        String [] listName = {"Студент ", " получил ", " по предмету "};
+        for (int i =0; i < arrayData.length; i++) {
+            String[] arrData = arrayData[i].split(":");
+            result.append(listName[i]);
+            result.append(arrData[1]);
+            }
+        return result;
     }
 }
